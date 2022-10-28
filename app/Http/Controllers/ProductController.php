1db78f098393ff649ac01eedbcc2f250fd0cc4c6
OscarLBT <115490970+OscarLBT@users.noreply.github.com>
2022-10-28 15:49:28 -0700

    Updated output to qty_p as qty

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -256,7 +256,7 @@ class ProductController extends Controller
         ->get();
 
         $product_lots = Quantity::orderBy('item', 'asc')
-        ->select('lot', 'qty_p')
+        ->select('lot', 'qty_p as [qty]')
         ->orderBy('qty_p', 'desc')
         ->orderBy('lot', 'asc')
         ->where('sku', '=', $id)
