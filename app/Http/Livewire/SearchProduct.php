f9a52b92d6eb9d9b9be359a7f3828017da39ad0a
Horacio Santoyo <hs2600@gmail.com>
2022-11-01 00:10:58 -0700

    hide private label items from search

--- app/Http/Livewire/SearchProduct.php
+++ app/Http/Livewire/SearchProduct.php
@@ -14,38 +14,57 @@ class SearchProduct extends Component
     public function render()
     {
         sleep(1);
-        $products = Product::where('description','like', '%'.$this->search.'%')
-                ->orWhere('item', 'like', '%'.$this->search.'%')
-                ->paginate(50);
-
-        $series = Collection::where('series','like', '%'.$this->search.'%')
-        ->where('category', '=', 'series')
-        ->limit(5)
-        ->get();
-
-        $productsAll = Product::where('description','like', '%'.$this->search.'%')
-                ->orWhere('item', 'like', '%'.$this->search.'%')
-                ->get();
-
-        $items = Product::where('item','=', $this->search)
+        $products = Product::where('series.status', '!=', 1)
             ->leftjoin('collections as series', function ($join) {
-            $join->on('products.material', '=', 'series.material')
-              ->On('products.series', '=', 'series.series')
-              ->where('series.category', '=', 'series');
+                $join->on('products.material', '=', 'series.material')
+                    ->On('products.series', '=', 'series.series')
+                    ->where('series.category', '=', 'series')
+                    ->where('series.status', '!=', 1);
             })
-            ->selectRaw('products.*, series.size_desc')
+            ->select(
+                'products.sku',
+                'products.item',
+                'products.description',
+                'products.qty_p as qty',
+                'products.uofm'
+            )
+            ->where(function ($query) {
+                $query
+                    ->where('products.description', 'like', '%' . $this->search . '%')
+                    ->orWhere('products.item', 'like', '%' . $this->search . '%');
+            })
+            ->paginate(50);
+
+        $series = Collection::where('series', 'like', '%' . $this->search . '%')
+            ->where('category', '=', 'series')
+            ->where('status', '!=', 1)
+            ->limit(5)
+            ->get();
+
+        $productsAll = Product::where('description', 'like', '%' . $this->search . '%')
+            ->orWhere('item', 'like', '%' . $this->search . '%')
+            ->get();
+
+        $items = Product::where('item', '=', $this->search)
+            ->leftjoin('collections as series', function ($join) {
+                $join->on('products.material', '=', 'series.material')
+                    ->On('products.series', '=', 'series.series')
+                    ->where('series.category', '=', 'series');
+            })
+            ->selectRaw('products.*')
+            ->where('series.status', '!=', 1)
             ->limit(1)
             ->get();
 
-        if($this->search == ''){
-            return view('livewire.search-product',['products' => $this->search]);
+        if ($this->search == '') {
+            return view('livewire.search-product', ['products' => $this->search]);
         } else {
             return view('livewire.search-product')
-            ->with(['products' => $products])
-            ->with(['items' => $items])
-            ->with('search', $this->search)
-            ->with(['series' => $series])
-            ->with('count', $productsAll->count())
-            ;
-    }}
+                ->with(['products' => $products])
+                ->with(['items' => $items])
+                ->with('search', $this->search)
+                ->with(['series' => $series])
+                ->with('count', $productsAll->count());
+        }
+    }
 }
