17cc7ed2c52383fade32b0b4ca93513f4a1653e7
OscarLBT <115490970+OscarLBT@users.noreply.github.com>
2022-11-01 13:15:59 -0700

    Create 2022_11_01_194023_create_customers.php
    Created customers table

---
+++ database/migrations/2022_11_01_194023_create_customers.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('customer',8)->unique();
+            $table->string('custname',50)->unique();
+            $table->string('salesrep',3);
+            $table->string('salesterr',3);
+            $table->string('pricelevel',8);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+};
