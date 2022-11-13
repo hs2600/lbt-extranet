23a46814435847fa11aea1b323d870db5b81a6c1
Horacio Santoyo <hs2600@gmail.com>
2022-11-12 17:38:15 -0800

    dealer locator update

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -21,25 +21,39 @@ class Controller extends BaseController
     {
         error_log("INFO: get /");
 
-        $field_list = 'min(abs('. $zip. '-zip)) as diff, customer, ship_to_name, address1, address2, city, state, zip, phone1';
+        if(isset($_GET['location'])){
+            $zip = $_GET['location'];
+        }
+        $error = '';
 
-        $showrooms = DB::table('addresses')
-        ->selectRaw($field_list)
-        ->groupBy('customer')
-        ->groupBy('ship_to_name')
-        ->groupBy('address1')
-        ->groupBy('address2')        
-        ->groupBy('city')
-        ->groupBy('state')
-        ->groupBy('zip')
-        ->groupBy('phone1') 
-        ->orderBy('diff')
-        ->limit(1)
-        ->get();
+        $latlon = DB::table('zip_lat_lon')
+            ->where('zip', '=', $zip)
+            ->first();
+
+        if (is_null($latlon) || strlen($zip) < 5) {
+            $error = 'invalid';
+            $showrooms = array();
+        } else {
+            $error = '';
+            $zip =  substr($latlon->zip, 0, 5);
+            $lat =  $latlon->lat;
+            $lon =  $latlon->lon;
+
+            $distance_calc = '(6371 * acos( cos( radians(' . $lat . ') ) * cos( radians( lat ) ) * cos( radians(' . $lon . ') - radians(lon) ) + sin( radians(' . $lat . ') ) * sin( radians(lat) ) ))';
+
+            $field_list = 'customer_name, address1, address2, city, state, zip, phone1, locator_priority, ifnull(' . $distance_calc . ',1) as distance, ifnull(' . $distance_calc . ',10) * locator_priority as distance_priority';
+
+            $showrooms = DB::table('addresses')
+                ->selectRaw($field_list)
+                ->orderBy('distance_priority')
+                ->limit(1)
+                ->get();
+        }
 
         return view('find-dealer.dealer_locator_zip')
         ->with('showrooms', $showrooms)
-        ->with('zip', $zip);
+        ->with('zip', $zip)
+        ->with('error', $error);
     }
 
     /**
@@ -67,11 +81,13 @@ class Controller extends BaseController
             $lat =  $latlon->lat;
             $lon =  $latlon->lon;
 
-            $field_list = 'ship_to_name, address1, address2, city, state, zip, lat, (6371 * acos( cos( radians(' . $lat . ') ) * cos( radians( lat ) ) * cos( radians(' . $lon . ') - radians(lon) ) + sin( radians(' . $lat . ') ) * sin( radians(lat) ) )) as distance';
+            $distance_calc = '(6371 * acos( cos( radians(' . $lat . ') ) * cos( radians( lat ) ) * cos( radians(' . $lon . ') - radians(lon) ) + sin( radians(' . $lat . ') ) * sin( radians(lat) ) ))';
+
+            $field_list = 'customer_name, address1, address2, city, state, zip, locator_priority, ifnull(' . $distance_calc . ',1) as distance, ifnull(' . $distance_calc . ',10) * locator_priority as distance_priority';
 
             $showrooms = DB::table('addresses')
                 ->selectRaw($field_list)
-                ->orderBy('distance')
+                ->orderBy('distance_priority')
                 ->limit(5)
                 ->get();
         }
