37a95cfeafbcee9824bfa32691a6c4fa3df9695d
Horacio Santoyo <hs2600@gmail.com>
2022-11-17 01:59:28 -0800

    add user role and company

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,8 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'company',
+        'role',
         'password',
     ];
 
@@ -41,4 +43,5 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    
 }
