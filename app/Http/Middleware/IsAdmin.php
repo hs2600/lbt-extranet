a91734ee3c805fd30af331e57e70f7457302fa42
Horacio Santoyo <hs2600@gmail.com>
2022-11-18 17:30:44 -0800

    spelling error

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -18,7 +18,7 @@ class IsAdmin
         //check if user is logged in
         //check if user is admin
         //check if user is internal (is using LBT email address)
-        //If any condition is false, send user to login page. If user is already logged in, they will be redirectd to main page (collections)
+        //If any condition is false, send user to login page. If user is already logged in, they will be redirected to main page (collections)
         if (!auth()->check() || (auth()->user()->role != 'admin' || strpos(auth()->user()->email, "lunadabaytile.com") === false)
         ) {
             return redirect(route('login'));
