e17799ac18b719e765603c6f5465bc1def36dc53
Horacio Santoyo <hs2600@gmail.com>
2022-12-16 16:49:49 -0800

    dealer locator marker update

--- resources/views/find-dealer/dealer_locator.blade.php
+++ resources/views/find-dealer/dealer_locator.blade.php
@@ -18,6 +18,15 @@
     position: relative;
     }
 
+    [type=checkbox]:checked,[type=radio]:checked {
+	background-color: #04403c;
+    }    
+
+    [type=checkbox],[type=radio] {
+	color: #cea29d;
+	background-color: #fff;
+    }    
+
 </style>
 
 <section class="section dashboard">
@@ -44,10 +53,10 @@
                     margin-bottom: 4px;
                     ">Update</button><br>
                     <input type="checkbox" name="JA" value="1" {{ $JA }}>
-                    <label for="vehicle1"> Jonathan Adler</label><br>
+                    <label for="JA"> Jonathan Adler</label><br>
 
                     <input type="checkbox" name="TB" value="1" {{ $TB }}>
-                    <label for="vehicle2"> Tommy Bahama</label><br>                    
+                    <label for="TB"> Tommy Bahama</label><br>                    
                 </form>
                 <span style="color: red;"><i>@if ($error == 'invalid') Please enter a valid US ZIP Code @endif
                     </i></span>
@@ -59,13 +68,15 @@
             <div class="card" style="margin-top: 20px;">
                 <div class="card-body" style="padding-bottom: 10px;">
                     <h5 class="card-title" style="padding: 8px 0 0px 0;">
-                        <span class="position-absolute top-0 left-0 translate-middle badge rounded-pill text-light" style="background-color: #04403c85;">
+                        <span class="position-absolute top-0 left-0 translate-middle badge rounded-pill text-light" style="background-color: #04403c99;">
                             {{ $loop->iteration }}</span>
                         {{ ucwords(strtolower($showroom->customer_name)) }}</h6>
                         <i><span style="font-size: 14px; color: #555;"><b>{{ round($showroom->distance,2) }}</b> miles away
                             </span></i><br>
                         <span class="card-text">{{ ucwords(strtolower($showroom->address1 . ' ' . $showroom->address2)) }}</span><br>
                         <span class="card-text">{{ ucwords(strtolower($showroom->city)) . ', ' . $showroom->state . '  ' . $showroom->zip }}</span>
+                        <br>
+                        <span class="card-text"><i>[test/ {{ $showroom->authorized }} ]</i></span>
                 </div>
             </div>
 
@@ -85,7 +96,7 @@
     </div>
 </section>
 
-<script src="https://maps.googleapis.com/maps/api/js?key={{ env('GOOGLE_MAPS_API_KEY') }}&callback=initMap&v=weekly" defer></script>
+<script src="https://maps.googleapis.com/maps/api/js?key={{ env('GOOGLE_MAPS_API_KEY') }}&v=beta&libraries=marker&callback=initMap" defer></script>
 
 <script>
     let map, markers = [];
@@ -156,20 +167,41 @@
 
 
     //Call this to place marker based on address
-    function codeAddress(address, label, distance, index) {
-        const svgMarker = {
-            path: "M10.453 14.016l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
-            fillColor: "blue",
-            fillOpacity: 0.6,
-            strokeWeight: 0,
-            rotation: 0,
+    function codeAddress(address, customer, distance, index) {
+
+        var pinColor = "#04403c";
+    
+        // Pick your pin (hole or no hole)
+        var pinSVGHole = "M12,11.5A2.5,2.5 0 0,1 9.5,9A2.5,2.5 0 0,1 12,6.5A2.5,2.5 0 0,1 14.5,9A2.5,2.5 0 0,1 12,11.5M12,2A7,7 0 0,0 5,9C5,14.25 12,22 12,22C12,22 19,14.25 19,9A7,7 0 0,0 12,2Z";
+        var labelOriginHole = new google.maps.Point(12,15);
+        var pinSVGFilled = "M 12,2 C 8.1340068,2 5,5.1340068 5,9 c 0,5.25 7,13 7,13 0,0 7,-7.75 7,-13 0,-3.8659932 -3.134007,-7 -7,-7 z";
+        var labelOriginFilled =  new google.maps.Point(12,9);
+
+
+        var markerImage = {  // https://developers.google.com/maps/documentation/javascript/reference/marker#MarkerLabel
+            path: pinSVGFilled,
+            anchor: new google.maps.Point(12,17),
+            fillOpacity: 1,
+            fillColor: pinColor,
+            strokeWeight: 2,
+            strokeColor: "white",
             scale: 2,
-            anchor: new google.maps.Point(15, 30),
+            labelOrigin: labelOriginFilled
         };
 
+        const svgMarker = {
+        path: "M10.453 14.016l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
+        fillColor: "#04403c",
+        fillOpacity: 1,
+        strokeWeight: 0,
+        rotation: 0,
+        scale: 2,
+        anchor: new google.maps.Point(15, 30),
+    };
+        
         const pin = "https://img.icons8.com/cotton/512/shipping-location.png";
 
-        const icon = {
+        const icon_ = {
             url: "http://maps.gstatic.com/mapfiles/ridefinder-images/mm_20_green.png", // url
             scaledSize: new google.maps.Size(0, 0), // scaled size
             opacity: 1,
@@ -177,18 +209,28 @@
             anchor: new google.maps.Point(0, 0) // anchor
         };
 
+        const icon = document.createElement("div");
+
+        //icon.innerHTML = '<i class="fa-solid fa-store"></i>';
+        icon.innerHTML = index;
+
+        // Change the background color.
+        const pinViewBackground = new google.maps.marker.PinView({
+        background: "#04403c",
+        borderColor: "#cea29d",
+        glyphColor: "#fff",
+        glyph: icon,
+        });
+
         geocoder.geocode({
             'address': address
         }, function(results, status) {
             if (status == google.maps.GeocoderStatus.OK) {
-                var marker = new google.maps.Marker({
+                var marker = new google.maps.marker.AdvancedMarkerView({
                     position: results[0].geometry.location,
-                    label: {
-                        color: "white",
-                        text: index,
-                        fontSize: '15px',
-                        className: 'marker-position'
-                    },
+                    title: customer,
+                    //icon: pin,
+                    content: pinViewBackground.element,
                     map
                 });
 
@@ -196,7 +238,7 @@
                     '<div id="content">' +
                     '<div id="siteNotice">' +
                     "</div>" +
-                    '<h5 id="firstHeading" class="firstHeading">' + label + '</h5>' +
+                    '<h5 id="firstHeading" class="firstHeading">' + customer + '</h5>' +
                     '<div id="bodyContent">' +
                     "<p style='font-size: 16px;'>" + address + "</p>" +
                     "</div>" +
